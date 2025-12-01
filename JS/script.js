179d1eb1c8499c5bc113ed62dacfe1a07404bab5
RICHARD HENRIQUE <richardhenriquedeabreuferreira@gmail.com>
2025-11-30 21:49:08 -0300

    Refactor clock update logic in script.js

--- JS/script.js
+++ JS/script.js
@@ -2,13 +2,26 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
-setInterval((){
+const relogio = setInterval((){
     let dataToday = new Date()
     let hr = dataToday.getHours();
     let min = dataToday.getMinutes();
     let seg = dataToday.getSeconds();
 
-    horas.textContent = hr < 10 ? '0' + hr : hr;
-    minutos.textContent = min < 10 ? '0' + min: min;
-    segundos.textContent = seg < 10 ? '0' + seg: seg;
-}, 1000);
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+    
+    if(hr < 10){
+        horas.textContent = '0' + hr;
+    }
+    if(min < 10){
+        minutos.textContent = '0' + min;
+    }
+    if(seg < 10){
+        segundos.textContent = '0' + seg;
+    }
+    
+});
+
+relogio()
